0c5422ff87f0c8faa9b228862a1a39d751177b4d
Lee Choon Siong <choonsiong.lee@gmail.com>
2021-01-04 02:21:22 +0800

    Auto commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" { // Matches only /, else returns 404 not found
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello World!"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	//w.Write([]byte("Display a specific snippet..."))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d... 🤔", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST") // Add an 'Allow: POST' header to the response header map
+		//w.WriteHeader(405)
+		//w.Write([]byte("Method not allowed"))
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/choonsiong/snippetbox
+
+go 1.15
