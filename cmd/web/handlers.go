90b894371bad5c66aaa0ea60a0c5f97d7478dcd7
Lee Choon Siong <choonsiong.lee@gmail.com>
2021-01-07 00:53:52 +0800

    Auto commit

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,18 +8,30 @@ import (
 	"strconv"
 )
 
+// Define a home handler function
 func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" { // Matches only /, else returns 404 not found
+	// Check if the current request URL path exactly  matches "/". If it
+	// doesn't, use the http.NotFound() function to send a 404 response
+	// to the client. We then return from the handler.
+	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
+	// Initialize a slice containing the paths to the files. Note that the
+	// home.page.tmpl file must be the *first* file in the slice.
 	files := []string {
-		"./ui/html/home.page.tmpl", // this must be first
+		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
 		"./ui/html/footer.partial.tmpl",
 	}
 
+	// Use the template.ParseFiles() function to read the files and store the templates
+	// in a template set. Notice that we can pass the slice of file paths as a
+	// variadic parameter.
+	// If there's an error, we log the detailed error message
+	// and use the http.Error() function to send a generic 500 Internal Server
+	// Error response to the user.
 	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
@@ -28,6 +40,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Then use the Execute() method on the template set to write the template
+	// content as the response body. The last parameter to Execute() represents
+	// any dynamic data that we want to pass in, which for now we'll leave as nil.
 	err = ts.Execute(w, nil)
 
 	if err != nil {
@@ -37,6 +52,10 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return an 404
+	// page not found response.
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
 	if err != nil || id < 1 {
@@ -49,10 +68,26 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not. Note
+	// that http.MethodPost is a constant equal to the string "POST".
 	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST") // Add an 'Allow: POST' header to the response header map
+		// Use the Header().Set() method to add a header to the response
+		// header map. The first parameter is the header name, and the second
+		// parameter is the header value.
+		// Note that changing the response header map after a call to w.WriteHeader()
+		// or w.Write() will have no effect on the headers that the user receives.
+		w.Header().Set("Allow", "POST")
+
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a message response body.
+		// We then return from the function so that the subsequent code is not
+		// executed.
 		//w.WriteHeader(405)
 		//w.Write([]byte("Method not allowed"))
+
+		// Use the http.Error() helper function to send a 405 status code
+		// and a string as the response body (instead of above two separate
+		// methods).
 		http.Error(w, "Method not allowed", 405)
 		return
 	}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,16 +15,28 @@ func main() {
 		port = "4000"
 	}
 
+	// Initialize a new servemux
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Create a file server which serves files out of the "./ui/static" directory.
+	// Note that the path given to the http.Dir() function is relative to the
+	// project directory root.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Printf("Starting server on :%v", port)
-	err := http.ListenAndServe(":" + port, mux)
+
+	// If http.ListenAndServe() returns an error we use the log.Fatal() function
+	// to log the error message and exit. Note that any error returned by
+	// http.ListenAndServe() is always non-nil.
+	err := http.ListenAndServe(":" + port, mux) // :port will listen on all available network interfaces
 	log.Fatal(err)
 }
