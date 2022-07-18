db9b33a5fc65456551f47f55c0ba0e8804f18536
choonsiong <43933306+choonsiong@users.noreply.github.com>
2022-07-18 22:46:38 +0800

    Auto commit

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -29,8 +29,6 @@ import (
 	"time"
 )
 
-// serverError helper writes an error message and stack trace to the errorLog,
-// then sends a generic 500 Internal Server Error response to the user.
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
@@ -43,16 +41,11 @@ func (app *application) serverError(w http.ResponseWriter, err error) {
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
-// clientError sends a specific status code and corresponding description to
-// the user.
 func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
 
-// isAuthenticated return true if the current request is from authenticated user,
-// otherwise return false.
 func (app *application) isAuthenticated(r *http.Request) bool {
-	//return app.session.Exists(r, "authenticatedUserID")
 	isAuthenticated, ok := r.Context().Value(contextKeyIsAuthenticated).(bool)
 	if !ok {
 		return false
@@ -61,8 +54,6 @@ func (app *application) isAuthenticated(r *http.Request) bool {
 	return isAuthenticated
 }
 
-// notFound is simply a convenience wrapper around clientError which sends a
-// 404 Not Found response to the user.
 func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
