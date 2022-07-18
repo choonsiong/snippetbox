186ed40d871d66aa9696ce93fa56f7ac8e318187
choonsiong <43933306+choonsiong@users.noreply.github.com>
2022-07-18 22:39:59 +0800

    Auto commit

--- pkg/models/mock/snippets.go
+++ pkg/models/mock/snippets.go
@@ -1,3 +1,23 @@
+/*
+MIT License
+Copyright (c) 2022 Lee Choon Siong
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package mock
 
 import (
@@ -5,9 +25,6 @@ import (
 	"time"
 )
 
-// Create a simple struct which implements the same methods as our production
-// mysql.SnippetModel, but have the methods return some fixed dummy data
-// instead.
 var mockSnippet = &models.Snippet{
 	ID:      1,
 	Title:   "An old silent pond",
@@ -22,7 +39,6 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 	return 2, nil
 }
 
-// Get returns a models.ErrNoRecord unless the snippet ID is 1.
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 	switch id {
 	case 1:
