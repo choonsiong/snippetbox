06ac08c3d1ed3eaff5087cf8ba0f126649551b51
choonsiong <43933306+choonsiong@users.noreply.github.com>
2022-07-18 22:54:45 +0800

    Auto commit

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -81,8 +81,6 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Create a defer function which will always be run in the event
-		// of a panic as Go unwinds the stack.
 		defer func() {
 			if err := recover(); err != nil {
 				w.Header().Set("Connection", "close")
