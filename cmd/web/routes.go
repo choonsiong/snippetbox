8645f13f1d2cfef2ccdd25e7136f240b7ff70b16
choonsiong <43933306+choonsiong@users.noreply.github.com>
2022-07-18 22:45:08 +0800

    Auto commit

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,3 +1,23 @@
+/*
+MIT License
+Copyright (c) 2022 Lee Choon Siong
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package main
 
 import (
@@ -8,14 +28,9 @@ import (
 )
 
 func (app *application) routes() http.Handler {
-	// A middleware chain containing our standard middleware which will
-	// be used for every request our application receives.
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-
-	// Middleware specific to dynamic application routes.
 	dynamicMiddleware := alice.New(app.session.Enable, noSurf, app.authenticate)
 
-	// For RESTful routing
 	mux := pat.New()
 
 	mux.Get("/", dynamicMiddleware.ThenFunc(app.home))
@@ -42,24 +57,9 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/ping", http.HandlerFunc(ping))
 
-	// To handle static contents
-	//fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Take the ui.Files embedded filesystem and convert it to a http.FS type so that
-	// it satisfies the http.FileSystem interface. We then pass that to the
-	// http.FileServer() function to create the file server handler.
 	fileServer := http.FileServer(http.FS(ui.Files))
 
-	//mux.Get("/static/", http.StripPrefix("/static", fileServer))
-
-	// Our static files are contained in the "static" folder of the ui.Files embedded
-	// filesystem. So, for example, our CSS stylesheet is located at
-	// "static/css/main.css". This means that we now longer need to strip the prefix
-	// from the request URL -- any requests that start with /static/ can just be passed
-	// directly to the file server and the corresponding static file will be served
-	// (so long as it exists).
 	mux.Get("/static/", fileServer)
 
-	// Return the standard middleware chain followed by the servemux
 	return standardMiddleware.Then(mux)
 }
