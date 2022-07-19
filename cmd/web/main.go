b0c192ebfb21066e8ba46d4033c2dbc44c822b9a
choonsiong <43933306+choonsiong@users.noreply.github.com>
2022-07-20 03:33:11 +0800

    Auto commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -82,8 +82,8 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
-	//err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
+	//err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
